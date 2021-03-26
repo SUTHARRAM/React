024ad8233d69cc8545c18a3751b0f65ac50e67e8
SUTHARRAM <rls42847@gmail.com>
2021-03-27 00:21:58 +0530

    component part 2

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,7 +1,20 @@
+import React, { Component } from 'react'; 
 import { Navbar, NavbarBrand } from 'reactstrap'; 
 import Menu from './components/MenuComponent'; 
 
-function App() {
+import { DISHES } from './shared/dishes'; 
+
+class App extends Component {
+
+  constructor(props){
+    super(props); 
+
+    this.state =  {
+      dishes: DISHES, 
+    }; 
+  }
+
+ render(){
   return (
     <div>
       <Navbar dark color="primary">
@@ -9,9 +22,11 @@ function App() {
           <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+ }
+  
 }
 
 export default App;
